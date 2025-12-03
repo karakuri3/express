197e2ee70d7a6349ffc9bd2fbc92bc1a0287c3f9
karakuri3 <senpingxingzuo@gmail.com>
2025-12-04 01:22:39 +0900

    add cat api

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes'); 
+var catRouter = require('./routes/cat');
 
 var app = express();
 
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/cat',catRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
