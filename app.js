879d3baf460453b7735f2c20fc7cd1cc222747b2
karakuri3 <senpingxingzuo@gmail.com>
2025-12-04 02:41:33 +0900

    add dog api

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var users = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes'); 
 var catRouter = require('./routes/cat');
+var dogRouter = require('./routes/dog')
 
 var app = express();
 
@@ -28,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/cat',catRouter)
+app.use('/cat',catRouter);
+app.use('/dog',dogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
